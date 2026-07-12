2ab9417391af0acfd814bc9960487eb6c04a41c9
karthiks-codes <140952498+karthiks-codes@users.noreply.github.com>
2026-07-12 22:32:51 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,29 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        k = len(s1)
+        if k > len(s2):
+            return False
+
+        s1Count = Counter(s1)
+
+        left = 0
+        for i in range(k,len(s2)+1):
+            print(s2[left:i])
+            if s1Count == Counter(s2[left:i]):
+                print(Counter(s2[left:i]))
+
+                return True
+            left += 1
+
+        
+        return False
+
+
+            
+            
+        
+
+
+
+        
