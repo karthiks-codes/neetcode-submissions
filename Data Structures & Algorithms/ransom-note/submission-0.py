0f2480e0401bbe00e030076bffb6ff73a2fc0aa2
karthiks-codes <140952498+karthiks-codes@users.noreply.github.com>
2026-07-25 22:59:08 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        r = Counter(ransomNote)
+        m = Counter(magazine)
+
+        for k in r:
+            if r[k] > m[k]:
+                return False
+
+        return True
+
+
+        
