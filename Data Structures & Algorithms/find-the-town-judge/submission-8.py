3d677a0e6cb0a93e650e2ebf6fadb109e80a85c3
karthiks-codes <140952498+karthiks-codes@users.noreply.github.com>
2026-08-28 22:31:06 +0530

    Add: find-the-town-judge - submission-8

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        people = set()
+        trustMap = defaultdict(set)
+        
+        for i in trust:
+            people.add(i[0])
+            people.add(i[1])
+            trustMap[i[0]].add(i[1])        
+        for i in people:
+            flag = True
+            if i not in trustMap.keys():
+                for j in trustMap.values():
+                    if i not in j:
+                        flag = False
+                if flag: 
+                    return i
+        return -1
+
+        
