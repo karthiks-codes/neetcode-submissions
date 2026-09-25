62582f5c29c14cf2476ee20d417484b002e772a6
Akhil Karthik Boddupalli <140952498+karthiks-codes@users.noreply.github.com>
2026-09-25 19:41:43 +0530

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        visited = {}
+        q = [node]
+        visited[node] = Node(node.val)
+        while q:
+            cur = q.pop(0) 
+            for n in cur.neighbors:
+                if n not in visited:
+                    visited[n] = Node(n.val)
+                    q.append(n)
+                visited[cur].neighbors.append(visited[n])
+
+        return visited[node]
+
+
+        
+
+        
+            
+
+        
+
+
+        
